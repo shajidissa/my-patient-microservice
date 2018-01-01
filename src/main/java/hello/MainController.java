24d85d16aeec80d932170da0a45db0e9b754d660
Shajid Issa <shajidissa@Shajids-MacBook-Pro.local>
2018-01-01 00:16:39 +0000

    error handling

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -1,26 +1,17 @@
 package hello;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 
 @Controller    // This means that this class is a Controller
@@ -68,6 +59,27 @@ public class MainController  {
 		
 		return "{}";
 	}
+	
+	@PostMapping(path = "/createpatient3")
+    public ResponseEntity <String> persistPerson(@Valid @RequestBody Patient patient) {
+        
+		
+		Patient n = new Patient();
+		n.setName(patient.getName());
+		n.setNhsid(patient.getNhsid());
+		
+		Patient p = patientRepository.save(n);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+		
+		/*
+		if (personService.isValid(person)) {
+            personRepository.persist(person);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+        */
+    }
 	
 	
 	@GetMapping(path="/all")
