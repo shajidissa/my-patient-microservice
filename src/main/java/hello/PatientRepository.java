75e2f5859896696ed7d920b14f42bf327ba0a9ab
Shajid Issa <saj.issa@gmail.com>
2018-01-08 02:44:29 +0000

    custom query

--- src/main/java/hello/PatientRepository.java
+++ src/main/java/hello/PatientRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
@@ -16,8 +17,8 @@ public interface PatientRepository extends CrudRepository<Patient, Long> {
 	public List<Patient> findById(int id);
 	
 	@Query("from Patient p where p.name like %:name%")
-	public List<Patient> findByName(String name);
+	public List<Patient> findByName(@Param("name") String name);
 	
 	@Query("from Patient p where p.nhsid like %:nhsid%")
-	public List<Patient> findByNhsid(String nhsid);
+	public List<Patient> findByNhsid(@Param("nhsid") String nhsid);
 }
