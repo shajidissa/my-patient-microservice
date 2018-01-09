6548c433bafa21d92c2b0c2de5592085ec24df18
Shajid Issa <saj.issa@gmail.com>
2018-01-09 01:03:54 +0000

    delete patient

--- src/main/java/hello/PatientRepository.java
+++ src/main/java/hello/PatientRepository.java
@@ -2,9 +2,11 @@ package hello;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
@@ -22,6 +24,8 @@ public interface PatientRepository extends CrudRepository<Patient, Long> {
 	@Query("from Patient p where p.nhsid like %:nhsid%")
 	public List<Patient> findByNhsid(@Param("nhsid") String nhsid);
 	
+	@Transactional
+	@Modifying
 	@Query("delete from Patient p where p.id = :id")
 	public void deleteById(@Param("id") int id);
 }
